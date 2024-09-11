a258fde01707d49a8f1eda7fbcb5a62262b15ca2
Anand <aanadmainwal@gmail.com>
2024-09-11 12:53:15 +0530

    Delete cart button now remove product Html

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchedProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -103,7 +104,10 @@ document.querySelectorAll('.js-delete-link')
   .forEach((link) =>{
     link.addEventListener('click', () =>{
       const productId = link.dataset.productId;
-      removeFromCart(productId); 
-      console.log(cart);          
+      removeFromCart(productId); //fun call/cart.js 
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      
+      container.remove(); 
     });
   });
