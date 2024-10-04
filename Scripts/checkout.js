cd82828f33e19e00bf6ab8562c2fbacb4c3b9752
aanandmainwal@gmail.com <aanadmainwal@gmail.com>
2024-10-04 20:01:59 +0530

    Create class version of cart

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkoutPage/orderSummary.js";
 import { renderPaymentSummary } from "./checkoutPage/paymentSummary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
