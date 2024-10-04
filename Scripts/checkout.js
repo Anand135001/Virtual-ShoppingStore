a319ee5181fb96d33a2ecb1109c0a499bbc1abcc
aanandmainwal@gmail.com <aanadmainwal@gmail.com>
2024-10-04 16:35:57 +0530

    Oop object file practice

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkoutPage/orderSummary.js";
 import { renderPaymentSummary } from "./checkoutPage/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
